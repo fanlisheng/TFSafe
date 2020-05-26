8df6135bc5b93f3971212f5d3a1fabcfb1397cb6
874112968@qq.com <874112968@qq.com>
2020-05-26 17:49:30 +0800

    1

--- TFSafe/Classes/NSDictionary+Safe.m
+++ TFSafe/Classes/NSDictionary+Safe.m
@@ -14,15 +14,26 @@
     [self SwizzlingMethod:@"initWithObjects:forKeys:count:" systemClassString:@"__NSPlaceholderDictionary" toSafeMethodString:@"initWithObjects_st:forKeys:count:" targetClassString:@"NSDictionary"];
 }
 -(instancetype)initWithObjects_st:(id *)objects forKeys:(id<NSCopying> *)keys count:(NSUInteger)count {
-    NSUInteger rightCount = 0;
+    id safeObjects[count];
+    id safeKeys[count];
+    NSUInteger j = 0;
     for (NSUInteger i = 0; i < count; i++) {
-        if (!(keys[i] && objects[i])) {
-            break;
-        }else{
-            rightCount++;
+        id key = keys[i];
+        id obj = objects[i];
+        //如果key或value有为空的情况，就跳过去
+        if (!key || !obj) {
+            /*
+             break是结束整个循环，而continue是结束本次循环（跳过下一步），
+             为了循环的继续，我们就必须选择continue.
+             */
+            continue;
         }
+        //每一个value对应一个key，这个是相互对应的，详见demo。
+        safeKeys[j] = key;
+        safeObjects[j] = obj;
+        j++;
     }
-    self = [self initWithObjects_st:objects forKeys:keys count:rightCount];
+    self = [self initWithObjects_st:objects forKeys:keys count:j];
     return self;
 }
 @end
